89a1fd019777f4d260ba8ea2e878eeb230be13a9
Rocky Sims <rocky.sims@gmail.com>
2017-09-10 00:05:34 -0700

    More cleanup.

--- yith/src/main.ts
+++ yith/src/main.ts
@@ -1,21 +1,9 @@
 /// <reference path="../../typings/index.d.ts" />
 
-import o from 'other';
-import s from 'folder/subFolder/sub';
+//import o from 'other';
+//import s from 'folder/subFolder/sub';
 
 export const loop = function(): void {
-	console.log('loop()');
-	console.log('x: ' + o.x);
-	s();
-
-	//let creeps: {
-	//	[creepName: string]: Creep;
-	//} = Game.creeps;
-
-
-
-	//act.go(creeps[0], target);
-	//creeps[0].doExt();
-
-	console.log('abc');
+	console.log('loop() called');
+	
 };

28d3149e996d071734fd844844aedf7676452658
Rocky Sims <rocky.sims@gmail.com>
2017-09-12 02:53:27 -0700

    Fixed major bug in getAll.ts file.

--- yith/src/getAll.ts
+++ yith/src/getAll.ts
@@ -1,23 +1,23 @@
 interface AllInterface {
+	tick: number,
 	rooms: Room[],
 	spawns: Spawn[],
 	creeps: Creep[]
 }
 
 let cachedAll: AllInterface;
-export default {
-	get: function(): AllInterface {
-		if (!cachedAll) {
-			cachedAll = {
-				rooms: Object.keys(Game.rooms)
-					.map(key => Game.rooms[key]),
-				spawns: Object.keys(Game.spawns)
-					.map(key => Game.spawns[key]),
-				creeps: Object.keys(Game.creeps)
-					.map(key => Game.creeps[key])
-			};
-		}
-
-		return cachedAll;
+export default function(): AllInterface {
+	if (!cachedAll || cachedAll.tick != Game.time) {
+		cachedAll = {
+			tick: Game.time,
+			rooms: Object.keys(Game.rooms)
+				.map(key => Game.rooms[key]),
+			spawns: Object.keys(Game.spawns)
+				.map(key => Game.spawns[key]),
+			creeps: Object.keys(Game.creeps)
+				.map(key => Game.creeps[key])
+		};
 	}
+
+	return cachedAll;
 }

858db805f30e06b1fa5a012e2607219b2ff40504
Rocky Sims <rocky.sims@gmail.com>
2017-09-12 02:54:13 -0700

    Made Action.ts actions use creep.say().

--- yith/src/util/Action.ts
+++ yith/src/util/Action.ts
@@ -1,19 +1,28 @@
 export default class Action {
 	static harvest(creep: Creep, source: Source) {
-		if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+		let result: number = creep.harvest(source);
+		if (result == ERR_NOT_IN_RANGE) {
 			creep.moveTo(source, {visualizePathStyle: {stroke: '#ffff00'}});
+		} else if (result != 0) {
+			creep.say('#' + result);
 		}
 	}
 
 	static collect(creep: Creep, container: Container) {
-		if (creep.transfer(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+		let result: number = creep.transfer(container, RESOURCE_ENERGY);
+		if (result == ERR_NOT_IN_RANGE) {
 			creep.moveTo(container, {visualizePathStyle: {stroke: '#00ffff'}});
+		} else if (result != 0) {
+			creep.say('#' + result);
 		}
 	}
 
 	static deliver(creep: Creep, container: Container) {
-		if (creep.transfer(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+		let result: number = creep.transfer(container, RESOURCE_ENERGY);
+		if (result == ERR_NOT_IN_RANGE) {
 			creep.moveTo(container, {visualizePathStyle: {stroke: '#0000ff'}});
+		} else if (result != 0) {
+			creep.say('#' + result);
 		}
 	}
 }
